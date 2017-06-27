2692577537f539791082dd25441f22839b8a3a22
drc-jdunn <jwdunn@datarecognitioncorp.com>
2017-06-27 17:31:44 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,7 +44,7 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd" :
-    path => '/usr/local/bin/cowsay',
+    path => '/usr/local/bin',
     creates => '/etc/motd',
   }
 }
