8e0bf8e91ef09e9c87be496a6bc486b85961c673
drc-jdunn <jwdunn@datarecognitioncorp.com>
2017-06-27 17:29:57 -0500

    Added exec and removed old code

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,12 +43,8 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-  file { '/etc/motd':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "Puppet Rocks!\n",
-}
-
+  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd" :
+    path => '/usr/local/bin/cowsay',
+    created => '/etc/motd',
+  }
 }
