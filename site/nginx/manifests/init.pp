aa9169be73c48efd3bf122a52faa332e948fa8e0
harrisonkyee <yeeharry@johndeere.com>
2017-06-27 16:56:43 -0700

    ensure nginx and www direcotry

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,13 @@
-
+class nginx {
+   package {"nginx":
+     ensure => present,
+     }
+   file {"/var/www":
+      Ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+      }
+ }
+     
+   
