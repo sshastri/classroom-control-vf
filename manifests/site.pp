83dce20fe7e4d129f13cb641acc6f60ec9698e5c
SJHSI <SJHSI@users.noreply.github.com>
2017-06-27 15:07:26 -0700

    modified /etc/motd for exercise 7.1.

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,13 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  
+  file { '/etc/motd':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    content => "For exercise 7.1!\n",
+  }
+  
 }
