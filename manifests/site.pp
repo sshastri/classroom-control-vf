b869acc03b9e8bef78c022d188e0ea40d4d665fe
drc-jdunn <jwdunn@datarecognitioncorp.com>
2017-06-27 16:41:10 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,12 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Puppet Rocks!\n",
+}
+
 }
