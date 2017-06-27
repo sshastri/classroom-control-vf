169fe5fd652cce4be3e4733cfd34b0f7bbb43b46
Josh Grancell <jgrancell@users.noreply.github.com>
2017-06-27 18:32:26 -0400

    Added exec to generate motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,11 +44,8 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   
-  file { '/etc/motd':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "Naptime!\n",
-}
+  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+    path    => '/usr/local/bin',
+    creates => '/etc/motd',
+  }
 }
