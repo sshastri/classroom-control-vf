aece41a45e18d690febddbdd7b6f27d660e3fe00
Aline Menezes <aline.menezes@icann.org>
2017-06-27 16:30:53 -0700

    Update site.pp
    Include users class

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,6 +44,7 @@ node default {
   #   class { 'my_class': }
   
   include role::classroom
+  include users
   
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
     path    => '/usr/local/bin/',
