024e7a6318a403313c014de83e0af1a1f4367227
Shobhna Shastri <shobhna@puppet.com>
2017-06-28 17:23:40 +0000

    Lab 8.4 Homework as a demo!

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,43 @@
+class nginx {
+
+  package { 'nginx' :
+    ensure =>  present,
+  }
+
+  file { '/var/www' :
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   =>  '0755',
+  }
+
+  file { '/var/www/index.html' :
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source =>  'puppet:///modules/nginx/index.html',
+  }
+
+  file { '/etc/nginx/nginx.conf' :
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source =>  'puppet:///modules/nginx/nginx.conf',
+  }
+
+  file { '/etc/nginx/conf.d/default.conf' :
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source =>  'puppet:///modules/nginx/default.conf',
+  }
+
+  service { 'nginx' :
+    ensure  => running,
+    enabled =>  true,
+  }
+
+}
