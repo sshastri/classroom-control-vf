c294fa178ab2990df394fbdd9275463c312b8a9c
Aline Menezes <aline.menezes@icann.org>
2017-06-28 11:45:00 -0700

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,18 @@
+class {
+  package { 'memcached':
+  ensure => present,
+  }
+  
+  file { 'Memcached File':
+  path => '/etc/sysconfig/memcached',
+  ensure => file,
+  source => 'puppet:///modules/memcached/memcached',
+  require => Package ['memcached'],
+  notify => Service ['memcached'],
+  }
+  
+  service { 'memcached':
+  ensure => running',
+  }
+
+}
