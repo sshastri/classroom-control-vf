ae25a98d6604211763b27d84a7123c119e935c14
Shobhna Shastri <shobhna@puppet.com>
2017-06-28 21:45:26 +0000

    Updated template

--- site/julyfourth/manifests/init.pp
+++ site/julyfourth/manifests/init.pp
@@ -2,11 +2,10 @@ class julyfourth {
 
   notify { "Party like it's 1776" : }
 
-  $var = 'Fireworks are amazing'
 
   file { '/tmp/networks' :
     ensure  => file,
-    content => epp('julyfourth/networks.epp'),
+    content => epp('julyfourth/networks.epp', { var => 'Fireworks are killer' }),
   }
 
 
