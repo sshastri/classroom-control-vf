f60a781652a1e95e7901dff946327bcdccb87ece
harrisonkyee <yeeharry@johndeere.com>
2017-06-28 09:52:15 -0700

    added skeleton class

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,9 @@
+class skeleton {
+   file {"/etc/skel":
+      ensure => directory,
+      }
+      
+   file {"/etc/skel/.bashrc":
+      ensure => present,
+      source => 'puppet:///sites/skeleton/bashrc'
+ }
