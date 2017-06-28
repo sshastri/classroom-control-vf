399ab8c1cbceae74fa63e97738cb837cf92f9e2a
Shobhna Shastri <shobhna@puppet.com>
2017-06-28 21:41:41 +0000

    Templates

--- site/julyfourth/manifests/init.pp
+++ site/julyfourth/manifests/init.pp
@@ -2,6 +2,8 @@ class julyfourth {
 
   notify { "Party like it's 1776" : }
 
+  $var = 'Fireworks are amazing'
+
   file { '/tmp/networks' :
     ensure  => file,
     content => epp('julyfourth/networks.epp'),
