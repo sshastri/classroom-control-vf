6c15ddccf166c7f5946855c288a6fe022ebe5099
Connor Hawley <connorhawley@users.noreply.github.com>
2017-06-28 11:06:39 -0400

    add config files

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -16,6 +16,25 @@ class nginx { 
     source => 'puppet:///modules/nginx/index.html',
   }
   
+  
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    source => 'puppet:///modules/nginx/nginx.conf',
+    owner   => 'root',    
+    group   => 'root',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+   }
+  
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    source => 'puppet:///modules/nginx/default.conf',
+    owner   => 'root',
+    group   => 'root',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  
   service { 'nginx' :
     ensure => running,
   }
