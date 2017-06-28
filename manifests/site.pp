a878831f0b3a793a9693da3631339d525038644f
Connor Hawley <connorhawley@users.noreply.github.com>
2017-06-28 09:31:04 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,7 +43,7 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-  include users
+  include users::example
 
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
     path => '/usr/bin:/usr/local/bin',
