ed4ed75f1538d308d16628b003c87e2c9b69b8f5
pcuenco <pcuenco@convokesystems.com>
2017-06-28 14:46:58 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,16 +1,16 @@
-class memcached {
+class memcached{
   package {'memcached':
     ensure  => present,
   }
 
-file {'/etc/sysconfig/memcached':
+file{'/etc/sysconfig/memcached':
   ensure   => file,
   owner    => 'root',
   group    => 'root',
   mode     => '0644',
   source   => 'puppet:///modules/memcached/memcached',
   require  => Package['memcached]',
-  }
+ }
 
 service{'memcached':
   ensure   => running,
