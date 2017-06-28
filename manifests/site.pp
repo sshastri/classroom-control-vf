4931ebeb43f0bf1570d6a83abe38118e4b73311b
drc-jdunn <jwdunn@datarecognitioncorp.com>
2017-06-28 13:50:45 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,6 +45,7 @@ node default {
   include role::classroom
   #include users
   include skeleton
+  include memcached
   #exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd" :
   #  path => '/usr/local/bin',
   #  creates => '/etc/motd',
