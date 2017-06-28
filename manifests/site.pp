528039ff4ad4d5ad0c79ff343797e8e6c676f3c4
pcuenco <pcuenco@convokesystems.com>
2017-06-28 18:10:47 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,6 +46,7 @@ node default {
   #include class::users
   include users
   include memcached
+  include aliases
   # include skeleton
 if $::virtual != 'physical'{
   $vmname = capitalize($::virtual)
