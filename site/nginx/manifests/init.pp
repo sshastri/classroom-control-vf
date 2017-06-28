19af64d06d795c48e11e96ddaf94a9499fe57d00
Aline Menezes <aline.menezes@icann.org>
2017-06-27 19:28:11 -0700

    Update init.pp
    Building nginx class

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -5,4 +5,17 @@ class nginx {
     ensure => present,
     }
 
+file { '/var/www/index.html':
+  ensure => file,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
+  require package ['nginx'],
+  source => 'puppet:///modules/nginx/index.html'
+   }
+   
+service {'nginx':
+  ensure => runnning,
+  enable => true,
+  }
 }
