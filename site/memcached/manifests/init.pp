b2222081bb9169f17b265a697fbe2f142afee40e
harrisonkyee <yeeharry@johndeere.com>
2017-06-28 11:46:23 -0700

    added memcached class

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,21 @@
+class memcached {
+   package {'memcached':
+     ensure => present,
+     }
+     
+   file {'/etc/sysconfig/memcached':
+     ensure => file,
+     owner => 'root'
+     group => 'root',
+     mode => '0644',
+     source => 'puppet:///modules/memcached/memcached',
+     }
+     
+   service {'memcached':
+     ensure  => running,
+     enable  => true,
+     require => Package['memcahced'],
+     }
+    
+}
+     
