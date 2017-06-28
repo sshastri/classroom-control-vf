e87bb1d94e5b3b2f70efdee8dcde7a38effada01
Aline Menezes <aline.menezes@icann.org>
2017-06-28 13:43:07 -0700

    Cleaning up the code

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -5,32 +5,29 @@ class nginx {
     ensure => present,
     }
     
+File {
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
+  }
+
 file { '/var/www/':
   ensure => directory,
   }
 
 file { '/var/www/index.html':
   ensure =>  present,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
   require => Package['nginx'],
   source => 'puppet:///modules/nginx/index.html',
    }
    
 file { '/etc/nginx/conf.d/default.conf':
   ensure =>  present,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
   source => 'puppet:///modules/nginx/default.conf',
    } 
    
  file { '/etc/nginx/nginx.conf':
   ensure =>  present,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
   source => 'puppet:///modules/nginx/nginx.conf',
    }    
    
