0a04ae811caa20f860b9e0cdd0c6ae93968274be
SJHSI <SJHSI@users.noreply.github.com>
2017-06-28 11:49:58 -0700

    Lab 11.1, add init.pp for class memcached

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,23 @@
+class memcached {
+  
+  package { 'memcached':
+    ensure => present,
+  }
+  
+  file { '/etc/sysconfig/memcached':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/memcached/memcached',
+    resuire => Package['memcached'],
+  }
+  
+  service { 'memcached':
+    ensure => present,
+    enable => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
+  
+  
+}
