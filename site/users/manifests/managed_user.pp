4d6bd2a222641f4fc22de88ec2b57e0f310800e9
Connor Hawley <connorhawley@users.noreply.github.com>
2017-06-29 13:49:39 -0400

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,21 +1,17 @@
 define users::managed_user(
   $user = $title,
-  Integer $gid,
+  String $group,
   ){
   user { $user:
     ensure      => present,
     managehome  => 'true',
     home        => "/home/${name}",
     managegroup => 'true',
-    gid         => $gid,
+    group       => $group,
   
   }
   
-  group { $gid:
-    ensure => present,
-    before => User[$name],
-  }
-  
+
   file { "/home/${user}/.ssh/":
     ensure  => directory,
     owner   =>  $user,
