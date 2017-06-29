d00ac4d58f91f02b02abafacecabdb3005d4540f
Connor Hawley <connorhawley@users.noreply.github.com>
2017-06-29 10:02:01 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,41 +1,70 @@
 class nginx { 
-  package { 'nginx':  
-    ensure  => present,  
-  }  
-  file { 'document':   
-    ensure  => directory,  
-    path    => '/var/www',
-    owner   => 'root',
-    group   => 'root',
+
+  case $facts['os']['family']{
+    'RedHat', 'Debian': {
+      $package          = 'nginx'
+      $owner            = 'root'
+      $group            = 'root'
+      $document_root    = '/var/www'
+      $config_dir       = '/etc/nginx/'
+      $svr_block_dir    = '/etc/nginx/conf.d'
+      $logs_dir         = '/var/log/nginx'
+    }
+    'Windows': {
+      $package          = 'nginx-service'
+      $owner            = 'Administrator'
+      $group            = 'Administrator'
+      $document_root    = 'C:/ProgramData/nginx/html'
+      $config_dir       = 'C:/ProgramData/nginx/'
+      $svr_block_dir    = 'C:/ProgramData/nginx/conf.d'
+      $logs_dir         = 'C:/ProgramData/nginx/logs'
+    }
+    default:{
+      fail("${facts['os']['family']} is not supported")
+      }
+
+  $user = $facts['os']['family'] ? {
+    'RedHat'  => 'nginx',
+    'Debian'  => 'www-data',
+    'Windows' => 'nobody',
+    default   => 'nginx',
+  }
+
+
+  File {
+  owner => $owner,
+  group => $group,
   }
   
-  file { '/var/www/index.html':
+  package {  $package:  
+    ensure  => present,  
+  }  
+  file { $document_root:   
+    ensure  => directory,  
+  }
+  
+  file { "${document_root}/index.html":
     ensure => file,
-    owner  => 'root',
-    group  => 'root',
     source => 'puppet:///modules/nginx/index.html',
   }
   
   
-  file { '/etc/nginx/nginx.conf':
+  file { "${config_dir}/nginx.conf":
     ensure => file,
-    source => 'puppet:///modules/nginx/nginx.conf',
-    owner   => 'root',    
-    group   => 'root',
-    require => Package['nginx'],
+    content_ => epp('nginx/nginx.conf.epp',
+    require => Package[$package],
     notify => Service['nginx'],
    }
   
-  file { '/etc/nginx/conf.d/default.conf':
+  file { "${svr_block_dir}/default.conf":
     ensure => file,
     source => 'puppet:///modules/nginx/default.conf',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['nginx'],
+    require => Package[$package],
     notify => Service['nginx'],
   }
   
   service { 'nginx' :
     ensure => running,
+    enable => true,
   }
 }
