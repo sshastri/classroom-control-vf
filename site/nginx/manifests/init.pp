df8bf2083fbb33eda6c9edbdae6694cf70612cb1
Shobhna Shastri <shobhna@puppet.com>
2017-06-29 16:35:35 +0000

    Lab 14.1 Homework

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,32 +1,70 @@
 class nginx {
 
-  package { 'nginx' :
+  case $::osfamily {
+    'RedHat', 'Debian' : {
+      $package  = 'nginx'
+      $owner    = 'root'
+      $group    = 'root'
+      $docroot  = '/var/www'
+      $confdir  = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir   = '/var/log/nginx'
+    }
+    'windows' : {
+      $package  = 'nginx-service'
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+      $docroot  = 'C:/ProgramData/nginx/html'
+      $confdir  = 'C:/ProgramData/nginx'
+      $blockdir = 'C:/ProgramData/nginx/conf.d'
+      $logdir   = 'C:/ProgramData/nginx/logs'
+    }
+    default : {
+      fail ( 'This operating system is not supported' )
+    }
+  }
+
+  #Need to figure out the user..
+  $user = $::osfamily ? {
+    'RedHat'  => 'nginx',
+    'Debian'  => 'www-data',
+    'windows' => 'nobody',
+    default   =>  'nginx',
+  }
+
+  package { $package :
     ensure =>  present,
   }
 
   File {
-    owner => 'root',
-    group => 'root',
+    owner => $owner,
+    group => $group,
     mode  => '0644',
   }
 
-  file { '/var/www' :
+  file { $docroot :
     ensure => directory,
   }
 
-  file { '/var/www/index.html' :
+  file { "${docroot}/index.html" :
     ensure => file,
     source =>  'puppet:///modules/nginx/index.html',
   }
 
-  file { '/etc/nginx/nginx.conf' :
-    ensure => file,
-    source =>  'puppet:///modules/nginx/nginx.conf',
-  }
+  # Todo : Change this to a template...
+  file { "${confdir}/nginx.conf" :
+    ensure  => file,
+    content =>  epp('nginx/nginx.conf.epp', {
+                                              user     => $user,
+                                              logdir   => $logdir,
+                                              confdir  => $confdir,
+                                              blockdir => $blockdir,
+                                            }),
+   }
 
-  file { '/etc/nginx/conf.d/default.conf' :
-    ensure => file,
-    source =>  'puppet:///modules/nginx/default.conf',
+  file { "${blockdir}/default.conf" :
+    ensure  => file,
+    content =>  epp( 'nginx/default.conf.epp', { docroot => $docroot } ),
   }
 
   service { 'nginx' :
