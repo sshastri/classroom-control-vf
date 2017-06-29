8cc578f080f9f852a1084e7de254654fe2fecd89
Shobhna Shastri <shobhna@puppet.com>
2017-06-29 13:37:53 -0700

    Updated nginx for parse error

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -4,17 +4,17 @@ class nginx {
   
   case $::os["family"] {
     'RedHat', 'Debian' : {
-      $owner    = 'root',
-      $group    = 'root',
-      $package  = 'nginx',
-      $doc_root = '/var/www',
+      $owner    = 'root'
+      $group    = 'root'
+      $package  = 'nginx'
+      $doc_root = '/var/www'
       }
       
      'Windows' : {
-      $owner    = 'Administrator',
-      $group    = 'Administrators',
-      $package  = 'nginx-service',
-      $doc_root = 'C:/ProgramData/nginx/html',
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+      $package  = 'nginx-service'
+      $doc_root = 'C:/ProgramData/nginx/html'
      }
   default: {
       fail ('This operating system is not supported.')
