ebecfd5a7e74196fea1d5924941f88c40050aeca
pcuenco <pcuenco@convokesystems.com>
2017-06-29 16:09:21 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,74 +1,63 @@
-class nginx {
-  case $facts['os']['family'] { 
-    'redhat','debian' : {  
+class nginx (
+  $root=undef,
+){
+  case $facts['os]['family']{
+    'redhat','debian':{
       $package = 'nginx'
-      $owner   = 'root'
-      $group   = 'root'
-      $docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $logdir  = '/var/log/nginx'
+      $owner = 'root'
+      $group = 'root'
+      $confdir ='/etc/nginx'
+      $logdir = '/var/log/nginx'
+      
+      $default_docroot = '/var/www'
     }
-     'windows' : {
-       $package ='nginx-service'
-       $owner = 'Administrator'
-       $group = 'Administrators'
-       $docroot = 'C:/ProgramData/nginx/html'
-       $confdir = 'C:/ProgramData/nginx'
-       $logdir = 'c:/ProgramData/nginx/logs'
-     }
-     default :{
-       fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-     }
-   }
-  
-  $user = $facts['os']['family'] ? {
-    'redhat'  => 'nginx',
-    'debian'  => 'www-data',
-    'windows' => 'nobody',
-   }  
-  
-   File { 
-     owner => $owner,
-     group => $group,
-     mode  => '0664',
-   }
-   
-   package{$package:
-     ensure => present,
-   }
-   
-   file{[$docroot,"${confdir}/conf.d"]:
-     ensure  => directory,
-   }
-   
-    file { "${docroot}/index.html":
-      ensure => file,
-      source => 'puppet:///modules/nginx/index.html',
-    }
-    
-     file { "${confdir}/nginx.conf":
-       ensure  => file,
-       content => epp('nginx/nginx.conf.epp',
-         { 
-           user    => $user, 
-           confdir => $confdir,  
-           logdir  => $logdir,  
-           }),
-        notify  => Service['nginx'], 
+      'windows':{
+        $package = 'nginx-service'
+        $owner = 'Administrator'
+        $group = 'Administrators'
+        $confdir ='C:/ProgramData/nginx'
+        $logdir = 'C:/ProgramData/nginx/logs'
+        
+        $default_docroot = 'C:/ProgramData/nginx/html'
+      }
+        default :{
+          fail("Module $(module_name) is not supported on ${facts['os']}")
+       }
       }
       
-      file { "${confdir}/conf.d/default.conf":
-        ensure  => file,
-        content => epp('nginx/default.conf.epp',
-          {
-             docroot => $docroot, 
-             }),
-         notify  => Service['nginx'], 
-       }
-       
-       service{'nginx':
-         ensure => running,
-         enable => true,
-       }
-   
-}
+      $port ='80'
+      
+      $user=$facts['os']['family']?{
+        'redhat' => 'nginx',
+        'debian' => 'www-data',
+        'windows' => 'nobody',
+      }
+      
+      $docroot=$root?{
+        undef   => $default_docroot,
+        default => $root,
+      }
+      
+      File{
+        owner  => $owner,
+        group  => $group,
+        mode   => '0644',
+      }
+      
+      package {$package:
+        ensure  => present,   
+      }
+      
+      file {[$docroot,"${confdir}/conf.d"]:
+        ensure  => directory,
+      }
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      }
