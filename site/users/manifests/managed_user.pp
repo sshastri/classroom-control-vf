87fdf652aab304808b64e77d96b5be0175e38823
Connor Hawley <connorhawley@users.noreply.github.com>
2017-06-29 13:21:12 -0400

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,1 +1,14 @@
-
+define users::managed_user(
+  $name, $group, $sshdir
+  ){
+  file {
+  user { $name:
+    managehome => 'true',
+    home   => "/home/${name}',
+    group  =>
+  
+  }
+}
+  
+  
+    
