28e71c1388932bd39cf2b206a0118c807fb95d92
harrisonkyee <yeeharry@johndeere.com>
2017-06-29 11:44:01 -0700

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,3 +1,5 @@
 class profile::base {
-  notify { "Hello, my name is ${::hostname}": }
+  #notify { "Hello, my name is ${::hostname}": }
+  mymessage = hiera('message')
+  notify { ${mymessage}:}
 }
